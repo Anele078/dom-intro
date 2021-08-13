f0d6a55e57de549141968d6dd64583181af06ace
Anele Silimela <silimela.anelle@gmail.com>
2021-08-13 20:35:42 +0200

    git status

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -11,3 +11,43 @@
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
 //link the function to a click event on the calculate button
+
+
+
+const billString = document.querySelector(".billString");
+const calculateBtn = document.querySelector(".calculateBtn");
+const billTotal = document.querySelector(".billTotal");
+
+calculateBtn.addEventListener("click", function(){
+
+    const billTotalElement = billTotal.value;
+    const billStringElement = billString.value;
+    const billItems = billStringElement.split(",");
+    var totalCost = 0;
+
+    for(var i=0; i<billItems.length; i++) {
+
+        if(billItems[i].includes('call')) {
+          totalCost +=2.75 ;
+      } else if (billItems[i].includes('sms')) {
+        totalCost += 0.65;
+
+      }
+
+    }
+
+    var roundedBillTotal = totalCost.toFixed(2);
+    totalCost.innerHTML = roundedBillTotal;
+  
+
+});
+
+
+
+
+        
+   
+
+
+
+
